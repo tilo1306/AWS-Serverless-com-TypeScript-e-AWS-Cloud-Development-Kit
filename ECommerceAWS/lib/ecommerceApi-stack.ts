e60a40cd5aceab5cd188ec354e45a394faea6c7d
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-11-01 22:49:35 -0300

    feat: Integrating API Gateway Lambda Function

--- ECommerceAWS/lib/ecommerceApi-stack.ts
+++ ECommerceAWS/lib/ecommerceApi-stack.ts
@@ -4,13 +4,23 @@ import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
 import * as cwLogs from 'aws-cdk-lib/aws-logs'
 import { Construct } from 'constructs'
 
+interface ECommerceApiStackProps extends cdk.StackProps{
+    productsFetchHandler: lambdaNodeJs.NodejsFunction
+
+}
+
 export class ECommerceApiStack extends cdk.Stack{
     
-    constructor(scope:Construct,id:string, props?: cdk.StackProps){
+    constructor(scope:Construct,id:string, props: ECommerceApiStackProps){
         super(scope,id,props)
 
         const api = new apiGateway.RestApi(this,"ECommerceApi",{
             restApiName:"ECommerceApi"
         })
+        const productsFetchIntegration = new apiGateway.LambdaIntegration(props.productsFetchHandler)
+
+        const productsResource = api.root.addResource("products")
+
+        productsResource.addMethod("GET", productsFetchIntegration)
     }
 }
