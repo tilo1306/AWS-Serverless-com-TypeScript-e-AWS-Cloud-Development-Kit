b7fd91c563634416eacbc1ad07464e954d03fd67
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-11-02 10:56:19 -0300

    fix: file path productFetchFunction.ts

--- ECommerceAWS/lib/productsApp-stack.ts
+++ ECommerceAWS/lib/productsApp-stack.ts
@@ -5,13 +5,13 @@ import * as cdk from 'aws-cdk-lib'
 import{Construct }from 'constructs'
 
 export class ProductsAppStack extends cdk.Stack{
-readonly productsFetchHandler: lambdaNodeJs.NodejsFunction
+    readonly productsFetchHandler: lambdaNodeJs.NodejsFunction
 
     constructor(scope:Construct,id: string,props?: cdk.StackProps){
         super(scope,id,props)
         this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, "ProductsFetchFunction",{
             functionName: "ProductsFetchFunction",
-            entry: "lambda/products/productsFetch.ts",
+            entry: "lambda/products/productsFetchFunction.ts",
             handler:"handler",
             memorySize: 128,
             timeout: cdk.Duration.seconds(5),
