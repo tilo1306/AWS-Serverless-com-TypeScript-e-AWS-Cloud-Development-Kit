5ef5d8e229e4e09923a21cc45fc2a4520419f293
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-11-01 22:23:15 -0300

    feat: create stack API Gateway

--- ECommerceAWS/lambda/products/ProductsFetchFunction.ts
+++ ECommerceAWS/lambda/products/ProductsFetchFunction.ts
@@ -2,7 +2,15 @@ import { APIGatewayProxyEvent,APIGatewayProxyResult,Context } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEvent, context: Context):Promise<APIGatewayProxyResult>{
     
+    const lambdaRequestId = context.awsRequestId
+
     const method =event.httpMethod
+
+    const apiRequestId = event.requestContext.requestId
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
+    
+
     if(event.resource === '/products'){
         if(method === 'GET'){
             console.log('GET');
---
+++ ECommerceAWS/lib/ecommerceApi-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib'
+import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
+import * as cwLogs from 'aws-cdk-lib/aws-logs'
+import { Construct } from 'constructs'
+
+export class ECommerceApiStack extends cdk.Stack{
+    
+    constructor(scope:Construct,id:string, props?: cdk.StackProps){
+        super(scope,id,props)
+
+        const api = new apiGateway.RestApi(this,"ECommerceApi",{
+            restApiName:"ECommerceApi"
+        })
+    }
+}
