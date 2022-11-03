7f44765d036ca08c0eeedb1073497660a1ac399e
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-11-03 20:08:36 -0300

    feat: create table products

--- ECommerceAWS/lib/ecommerceApi-stack.ts
+++ ECommerceAWS/lib/ecommerceApi-stack.ts
@@ -18,6 +18,7 @@ export class ECommerceApiStack extends cdk.Stack{
 
         const api = new apiGateway.RestApi(this,"ECommerceApi",{
             restApiName:"ECommerceApi",
+            cloudWatchRole:true,
             deployOptions:{
                 accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
                 accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
