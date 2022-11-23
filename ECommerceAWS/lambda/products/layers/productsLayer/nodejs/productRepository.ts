50f64013e8a1331449dc5948db27d0ecc63891e3
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-11-22 22:31:48 -0300

    feat: update product productRepository

--- ECommerceAWS/lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ ECommerceAWS/lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -64,4 +64,24 @@ export class ProductRepository {
             throw new Error('Product not found')
         }
     }
+
+    async updateProduct(productId:string, product:Product):Promise<Product>{
+        const data = await this.ddbClient.update({
+            TableName: this.productsDdb,
+            Key: {
+                id:productId
+            },
+            ConditionExpression: 'attribute_exists(id)',
+            ReturnValues: 'UPDATED_NEW',
+            UpdateExpression: 'set productName = :n, code = :c, price =:p, model = :m',
+            ExpressionAttributeValues:{
+                ':n': product.productName,
+                ':c': product.code,
+                ':p': product.price,
+                ':m': product.model
+            }
+        }).promise()
+        data.Attributes!.id = productId
+        return data.Attributes as Product
+    }
 }
