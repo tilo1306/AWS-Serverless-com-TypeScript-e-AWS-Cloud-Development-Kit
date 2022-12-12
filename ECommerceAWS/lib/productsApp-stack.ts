b1cadad0d5f5467d41fe0e5ea6130b367f4b90b1
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-12-12 15:43:06 -0300

    feat: cloudWatch add project

--- ECommerceAWS/lib/productsApp-stack.ts
+++ ECommerceAWS/lib/productsApp-stack.ts
@@ -47,7 +47,9 @@ export class ProductsAppStack extends cdk.Stack {
                PRODUCTS_DDB: this.productsDdb.tableName
             }, 
             layers: [productsLayer],
-            tracing: lambda.Tracing.ACTIVE
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
+
          })
       this.productsDdb.grantReadData(this.productsFetchHandler)
 
@@ -66,7 +68,8 @@ export class ProductsAppStack extends cdk.Stack {
                PRODUCTS_DDB: this.productsDdb.tableName
             },
             layers: [productsLayer],
-            tracing: lambda.Tracing.ACTIVE
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
          }) 
       this.productsDdb.grantWriteData(this.productsAdminHandler)
    }
