c8f8d3246c3ebd2ed799a08f5c47d945b9985d73
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-12-11 22:16:07 -0300

    feat: activate x-ray lambda

--- ECommerceAWS/lib/productsApp-stack.ts
+++ ECommerceAWS/lib/productsApp-stack.ts
@@ -46,7 +46,8 @@ export class ProductsAppStack extends cdk.Stack {
             environment: {
                PRODUCTS_DDB: this.productsDdb.tableName
             }, 
-            layers: [productsLayer]
+            layers: [productsLayer],
+            tracing: lambda.Tracing.ACTIVE
          })
       this.productsDdb.grantReadData(this.productsFetchHandler)
 
@@ -64,7 +65,8 @@ export class ProductsAppStack extends cdk.Stack {
             environment: {
                PRODUCTS_DDB: this.productsDdb.tableName
             },
-            layers: [productsLayer]
+            layers: [productsLayer],
+            tracing: lambda.Tracing.ACTIVE
          }) 
       this.productsDdb.grantWriteData(this.productsAdminHandler)
    }
